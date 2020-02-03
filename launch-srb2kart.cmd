61f082a75293ca133a9d8148aa378ecab6103dd6
James Mills <jbmills1109@gmail.com>
2020-02-03 00:04:33 -0500

    02/02/2020 changes
    - Added enumeration search mode.

--- launch-srb2kart.cmd
+++ launch-srb2kart.cmd
@@ -3,6 +3,7 @@
 REM Kurzov's Command-Line SRB2Kart Launcher
 REM Launcher Copyright (C)      2020 by James 'Kurzov' Mills
 REM SRB2Kart Copyright (C) 1998-2018 by Kart Krew and Sonic Team Junior
+REM HashDeep is a Public Domain program
 REM INI file processing code from Batchography book, written by Elias Bachaalany
 REM THIS PROGRAM COMES WITH NO WARRANTY, BE IT IMPLIED OR EXPLICIT. USE AT YOUR OWN RISK.
 
@@ -10,16 +11,32 @@ REM THIS PROGRAM COMES WITH NO WARRANTY, BE IT IMPLIED OR EXPLICIT. USE AT YOUR 
 
 :main
   @echo off
+  
+  setlocal enabledelayedexpansion
+  cd "%~dp0"
+  mkdir logs
+  REM For determining versions when doing hash-checking
+  set vanillahash=VANILLA
+  set fkarthash=FKART
+  set battlehash=BATTLE
+  set shaderhash=SHADER
+  REM End hash-checking placeholder names
+  set scriptlocation=%~dp0
+  set launcherlogging=%scriptlocation%\logs\
+  set temporaryhashstorage=%launcherlogging%\tempkarthash.txt
+  
   cls
   echo CONFIGURATION CHECK
-  setlocal enabledelayedexpansion
   call :get-ini kartlauncher.ini LAUNCHER CHECK-WORKING-DIRECTORIES cwd-enabled
-  if %cwd-enabled%==0 echo Automatic searching of working directories for SRB2Kart executables is: DISABLED.
-  if %cwd-enabled%==1 echo Automatic searching of working directories for SRB2Kart executables is:  ENABLED.
+  if %cwd-enabled%==0 echo Search mode is: MANUAL OPTION FILE.
+  if %cwd-enabled%==1 echo Search mode is:   HARDCODED LOOKUP.
+  if %cwd-enabled%==2 echo Search mode is:        ENUMERATION.
+  if not exist %scriptlocation%\hashdeep.exe if %cwd-enabled%==2 goto hashdeep-missing
   call :get-ini kartlauncher.ini LAUNCHER 32-BIT-DIR x32bitdir
   echo 32-bit (x86) working directory is %x32bitdir%
   call :get-ini kartlauncher.ini LAUNCHER 64-BIT-DIR x64bitdir
   echo 64-bit (x64) working directory is %x64bitdir%
+  if %cwd-enabled%==2 goto newsearch
   
   call :get-ini kartlauncher.ini LAUNCHER VANILLA-X86-PATH vanilla32
   if %cwd-enabled%==1 if "%vanilla32%"=="" echo 32-bit Kart install not found, checking 32-bit working directory...
@@ -36,11 +53,11 @@ REM THIS PROGRAM COMES WITH NO WARRANTY, BE IT IMPLIED OR EXPLICIT. USE AT YOUR 
   if not "%vanilla64%"=="" echo 64-bit Kart install is located at %vanilla64%
   
   call :get-ini kartlauncher.ini LAUNCHER FKART-X86-PATH fickart32
-  if %cwd-enabled%==1 if "%fickart32%"=="" echo 32-bit FKart install not found, checking 32-bit working directory...
-  if %cwd-enabled%==0 if "%fickart32%"=="" echo 32-bit FKart install not found
-  if %cwd-enabled%==1 if exist %x32bitdir%\SRB2FKART.EXE set fickart32=%x32bitdir%\SRB2FKART.EXE
-  if %cwd-enabled%==1 if "%fickart32%"=="" echo 32-bit FKart install still not found
-  if not "%fickart32%"=="" echo 32-bit FKart install is located at %fickart32%
+  if %cwd-enabled%==1 if "%fickart32-reg%"=="" echo 32-bit FKart install not found, checking 32-bit working directory...
+  if %cwd-enabled%==0 if "%fickart32-reg%"=="" echo 32-bit FKart install not found
+  if %cwd-enabled%==1 if exist %x32bitdir%\SRB2FKART.EXE set fickart32-reg=%x32bitdir%\SRB2FKART.EXE
+  if %cwd-enabled%==1 if "%fickart32-reg%"=="" echo 32-bit FKart install still not found
+  if not "%fickart32-reg%"=="" echo 32-bit FKart install is located at %fickart32-reg%
   
   call :get-ini kartlauncher.ini LAUNCHER FKART-X64-PATH fickart64
   if %cwd-enabled%==1 if "%fickart64%"=="" echo 64-bit FKart install not found, checking 64-bit working directory...
@@ -76,7 +93,7 @@ REM THIS PROGRAM COMES WITH NO WARRANTY, BE IT IMPLIED OR EXPLICIT. USE AT YOUR 
   if %cwd-enabled%==1 if exist %x64bitdir%\SRB2KART-BATTLEROYALE.EXE set fornite64=%x64bitdir%\SRB2KART-BATTLEROYALE.EXE
   if %cwd-enabled%==1 if "%fornite64%"=="" echo 64-bit Battle Royale install still not found
   if not "%fornite64%"=="" echo 64-bit Battle Royale install is located at %fornite64%
-  
+  :notice
   choice /N /T 5 /D Y /M "Is this okay?"
   cls
   call :get-ini kartlauncher.ini LAUNCHER HYUUSEEKER-ENABLED hs-enabled
@@ -96,16 +113,20 @@ REM THIS PROGRAM COMES WITH NO WARRANTY, BE IT IMPLIED OR EXPLICIT. USE AT YOUR 
   echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   if not "%vanilla32%"=="" echo 1: Vanilla, 32-bit
   if not "%vanilla64%"=="" echo 2: Vanilla, 64-bit
-  if not "%fickart32%"=="" echo 3: FKart, 32-bit
+  if not "%fickart32-reg%"=="" echo 3: FKart, 32-bit
   if not "%fickart64%"=="" echo 4: FKart, 64-bit
   if not "%shaders32%"=="" echo 5: Shader Fix, 32-bit
   if not "%shaders64%"=="" echo 6: Shader Fix, 64-bit
   if not "%fornite32%"=="" echo 7: Battle Royale, 32-bit
   if not "%fornite64%"=="" echo 8: Battle Royale, 64-bit
+  if not "%fickart32-sdr%"=="" echo 9: FKart with Shaders, 32-bit
+  if not "%fickart32-bry%"=="" echo A: FKart with Battle Royale, 32-bit
   echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  if "%lsfunction%"=="" choice /C:12345678Z /N /M "Choose an executable from the above list, or press Z to stop this program."
-  if "%lsfunction%"=="1" choice /C:12345678Z /N /M "Choose an executable from the above list, or press Z to stop this program. You may need to press Z more than once to completely halt the script."
-  if ERRORLEVEL 9 GOTO :eof
+  if "%lsfunction%"=="" choice /C:123456789AZ /N /M "Choose an executable from the above list, or press Z to stop this program."
+  if "%lsfunction%"=="1" choice /C:123456789AZ /N /M "Choose an executable from the above list, or press Z to stop this program. You may need to press Z more than once to completely halt the script."
+  if ERRORLEVEL 11 GOTO :eof
+  if ERRORLEVEL 10 GOTO fkart-bry32
+  if ERRORLEVEL 9 GOTO fkart-sdr32
   if ERRORLEVEL 8 GOTO br64
   if ERRORLEVEL 7 GOTO br32
   if ERRORLEVEL 6 GOTO shader64
@@ -191,8 +212,8 @@ REM ------------------------- FICKLE KART 32 BIT --------------------------
   echo Please wait for the game to start...
   echo WARNING: Window may appear below this console window!!
   echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  if ERRORLEVEL == 2 %fickart32% -connect %lastserver%
-  if ERRORLEVEL == 1 %fickart32% -openGL -connect %lastserver%
+  if ERRORLEVEL == 2 %fickart32-reg% -connect %lastserver%
+  if ERRORLEVEL == 1 %fickart32-reg% -openGL -connect %lastserver%
   goto menu
   
   :fkart32-indirect
@@ -202,8 +223,8 @@ REM ------------------------- FICKLE KART 32 BIT --------------------------
   echo Please wait for the game to start...
   echo WARNING: Window may appear below this console window!!
   echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  if ERRORLEVEL == 2 %fickart32% 
-  if ERRORLEVEL == 1 %fickart32% -openGL
+  if ERRORLEVEL == 2 %fickart32-reg% 
+  if ERRORLEVEL == 1 %fickart32-reg% -openGL
   goto menu
 REM ----------------------- FICKLE KART 64 BIT -----------------------------
   :fkart64
@@ -346,8 +367,25 @@ REM ---------------- BATTLE ROYALE 64 BIT -----------------------------------
   if ERRORLEVEL == 2 %fornite64% 
   if ERRORLEVEL == 1 %fornite64% -openGL
   goto menu 
+  
+  :fkart-bry32
+  echo FKART PLUS BATTLE ROYALE, 32 BIT
+  echo sorry, not implemented
+  pause
+  goto menu
+  
+  :fkart-sdr32
+  echo FKART PLUS SHADERS, 32 BIT
+  echo sorry, not implemented
+  pause
+  goto menu
+  
+REM Supporting Elements (the INI reader, """""Last Server""""" implementation, etc.)
 
-
+:hashdeep-missing
+cls
+echo Hashdeep is required to use the Command Line Interface. You can obtain it at http://md5deep.sourceforge.net/ - aborting program
+exit /B 1
 
 :get-ini <filename> <section> <key> <result>
   set %~4=
@@ -384,3 +422,17 @@ REM ---------------- BATTLE ROYALE 64 BIT -----------------------------------
   	if "%lastserver%"=="" goto %1-indirect
   	if not "%lastserver%"=="" goto %1-direct
   endlocal
+  
+:newsearch
+FOR /F "tokens=*" %%G IN ('DIR/B /S %x32bitdir%\*.exe') DO ( hashdeep -c md5 %%G >> "%temporaryhashstorage%" && FOR /F "usebackq tokens=2 skip=5 delims=," %%H IN ("%temporaryhashstorage%") DO ( FOR /F "tokens=1,2,3,4 delims=," %%I IN (checksums-x86.txt) DO (
+			if %%H==%%I if %%J==%vanillahash% set vanilla32=%%G
+			if %%H==%%I if %%J==%battlehash% if %%K==%vanillahash% set fornite32=%%G
+			if %%H==%%I if %%J==%shaderhash% if %%K==%vanillahash% set shader32=%%G
+			if %%H==%%I if %%J==%fkarthash% if %%K==%vanillahash% set fickart32-reg=%%G
+			if %%H==%%I if %%J==%fkarthash% if %%K==%shaderhash% if %%L==%vanillahash% set fickart32-sdr=%%G
+			if %%H==%%I if %%J==%fkarthash% if %%K==%battlehash% if %%L==%vanillahash% set fickart32-bry=%%G
+			if NOT %%H==%%I echo %%G >> %launcherlogging%\unknown-checksums-x86.txt
+		)
+	)
+)
+goto notice
