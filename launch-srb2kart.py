9d853c4729a2dec88157f2ade3d6d8f9599d6505
James Mills <jbmills1109@gmail.com>
2020-02-09 10:50:11 -0500

    02/09/2020 update
    - Python now has parity with the old batch script.

--- launch-srb2kart.py
+++ launch-srb2kart.py
@@ -3,111 +3,111 @@
 # SRB2Kart Copyright (C) 1998-2018 by Kart Krew & Sonic Team Junior
 # THIS PROGRAM COMES WITH NO WARRANTY, BE IT IMPLIED OR EXPLICIT. USE AT YOUR OWN RISK.
 
+import os
+import subprocess
+
 # Default options, will be externally read when I figure out how to read files.
-searchmode = "LEGACY"
-# searchmode help:
-#	LEGACY: Manual lookup
-#	LOOKUP: Search in defined directories
-#	ENUMERATE: Enumerate all found .EXEs
 hyuuseekerenabled = False
 # hyuuseekerenabled help:
 #	False: HyuuSeeker support is disabled
 #	True: HyuuSeeker support is enabled
-vanillax86path = ""
-# definition of x86 vanilla build, used if searchmode is 0
-vanillax64path = ""
-# definition of x64 vanilla build, used if searchmode is 0
-fkartx86path = ""
-# definition of x86 fkart build, used if searchmode is 0
-fkartx64path = ""
-# definition of x64 fkart build, used if searchmode is 0
-shadersx86path = ""
-# definition of x86 shader fix build, used if searchmode is 0
-shadersx64path = ""
-# definition of x64 shader fix build, used if searchmode is 0
-battleroyalex86path = ""
-# definition of x86 battle royale build, used if searchmode is 0
-battleroyalex64path = ""
-# definition of x64 battle royale build, used if searchmode is 0
 x32bitdir = "C:/USERS/KURZOV/DOWNLOADS/KART-X86"
-# definition of directory where 32bit executables are located, used if searchmode is 1
-x64bitdir = ""
-# definition of directory where 64bit executables are located, used if searchmode is 1
-md5path = "MD5DEEP.EXE"
-# Custom path to MD5DEEP (used in the ENUMERATION search mode).
+# definition of directory where 32bit executables are located
+x64bitdir = "C:/USERS/KURZOV/DOWNLOADS/KART-X64"
+# definition of directory where 64bit executables are located
 
-# Hey CLIde, this is my Kart build(s). Don't mess it up, please!
-class KartKnownBuild:
-    def __init__(self, bits, nameoffile, checksum, builddate, sourcemods):
-        self.bits = bits
-        self.nameoffile = nameoffile
-        self.checksum = checksum
-        self.builddate = builddate
-        self.sourcemods = sourcemods
-    
-    def sendbuildinfo(self, parameter_list):
-        raise NotImplementedError
-        # TODO: IMPLEMENTATION
+EXEnum = 0 # global var
+EXEcount = 0 # global var
+menuLoadedCount = 1 # global var
+availableexes = []
+availablefilenames = []
+available64exes = []
+available64filenames = []
+allbuilds = {}
 
-    def getbuildinfo(self):
-        raise NotImplementedError
-        # TODO: IMPLEMENTATION
+def i_am_a_build(chksm, filepath):
+    allbuilds[chksm] = filepath
 
-class buildNumIterator:
-    def __iter__(self):
-        self.a = 0
-        return self
+def lookforexecutables():
+    for filename in os.listdir(x32bitdir):
+        if filename.endswith(".exe"):
+            global EXEnum
+            availableexes.append(x32bitdir + "/" + filename)
+            availablefilenames.append(filename)
+            EXEnum += 1
+            continue
+        else:
+            continue
+    for filename in os.listdir(x64bitdir):
+        if filename.endswith(".exe"):
+            available64exes.append(x64bitdir + "/" + filename)
+            available64filenames.append(filename)
+            EXEnum += 1
+            continue
+        else:
+            continue
+    else:    
+        availableexes.extend(available64exes)
+        availablefilenames.extend(available64filenames)
 
-    def __next__(self):
-        x = self.a
-        self.a += 1
-        return x
-
-buildNumber = buildNumIterator()
-buildNumberIterator = iter(buildNumber)
-
-EXEnum = 0
-EXEcount = 0
-available32exes = []
-available32filenames = []
-unknownexes = {}
-
-def gimme_my_build_info(chksm, filepath):
-    if chksm=="65d0acd26180a9b3d784c889a199900f":
-        print("found v1.1 vanilla x86 EXE")
-        vanilla32 = KartKnownBuild(32,filepath,chksm,"05-29-2019",None)
+def definemybuilds():
+    for x in availableexes:
+        global EXEnum
+        global EXEcount
+        EXEcount += 1
+        subprocess.run('md5deep64.exe -W TEMPHASHDEEP.TXT ' + x,shell=True)
+        htempf = open("TEMPHASHDEEP.TXT")
+        tempchecksum = (htempf.read(32))
+        i_am_a_build(tempchecksum,x)
+        continue
     else:
-        print("found unknown executable")
-        unknownexes.add(filepath)
-
-
-        
+        print("COMPLETED ENUMERATING EXES")
 
 # MAIN PROGRAM
 
+lookforexecutables()
+definemybuilds()
 
-import os
-import subprocess
-
-for filename in os.listdir(x32bitdir):
-    if filename.endswith(".exe"):
-        available32exes.append(x32bitdir + "/" + filename)
-        available32filenames.append(filename)
-        EXEnum += 1
-        continue
-    else:
-        continue
-# adds .exe file names to AVAILABLEEXES list
-
-for x in available32exes:
-    if EXEnum < EXEcount:
-        EXEcount += 1
-        subprocess.run('md5deep.exe -W TEMPHASHDEEP.TXT ' + x,shell=True)
-        htempf = open("TEMPHASHDEEP.TXT")
-        tempchecksum = (htempf.read(32))
-        gimme_my_build_info(tempchecksum,x)
-        continue
-    else:
-        continue
+print("Kurzov\'s SRB2Kart Command-Line Launcher")
+for x, y in allbuilds.items():
+    print(str(menuLoadedCount) + ": " + y + " (checksum " + x + ")")
+    menuLoadedCount += 1
 else:
-    print("Completed enumerating EXEs")
+    menuSelection = input("Enter a build. ")
+    if int(menuSelection) < 0 or int(menuSelection) >= menuLoadedCount:
+        raise TypeError("Not a valid selection.")
+    print("Build selected was: " + menuSelection)
+    selectedBuild = availableexes[(int(menuSelection) - 1)]
+    print("Selected build was: " + selectedBuild)
+    if selectedBuild not in available64exes:
+        os.chdir(x32bitdir)
+    else:
+        os.chdir(x64bitdir)
+    OGLSelection = input("Would you like to use OpenGL? [y/n] ")
+    if OGLSelection[0] == 'y':
+        renderMode = "OPENGL"
+    elif OGLSelection[0] == 'n':
+        renderMode = "SOFTWARE"
+    else:
+        raise TypeError("Not a valid option.")
+    DirectConnect = input("Would you like to directly connect to a server? If so, please type its IP or web address here. ")
+    if DirectConnect == '' and renderMode == "OPENGL":
+        try:
+            subprocess.run(selectedBuild +' -opengl')
+        except OSError:
+            print("Something went wrong. Please submit a bug report.")
+    elif DirectConnect != '' and renderMode == "OPENGL":
+        try:
+            subprocess.run(selectedBuild +' -connect ' + DirectConnect + ' -opengl')
+        except OSError:
+            print("Something went wrong. Please submit a bug report.")
+    elif DirectConnect == '' and renderMode == "SOFTWARE":
+        try:
+            subprocess.run(selectedBuild)
+        except OSError:
+            print("Something went wrong. Please submit a bug report.")
+    elif DirectConnect != '' and renderMode == "SOFTWARE":
+        try:
+            subprocess.run(selectedBuild +' -connect ' + DirectConnect)
+        except OSError:
+            print("Something went wrong. Please submit a bug report.")
